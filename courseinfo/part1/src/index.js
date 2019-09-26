71d3a7081f8b08374acf6684a467380ba22a36a2
snehasirure <siruresneha@gmail.com>
2019-09-26 12:27:46 +0200

    Extracted part to separate constant

--- courseinfo/part1/src/index.js
+++ courseinfo/part1/src/index.js
@@ -11,11 +11,11 @@ const App = () => {
   const exercises3 = 14
 
   return (
-      <>
+    <>
       <Header coursename={coursename} />
       <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-  </>
+    </>
   )
 }
 
@@ -32,15 +32,9 @@ const Header = (props) => {
   const Content = (props) => {
     return (
       <div>
-        <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+        <Part part={props.part1} exercises={props.exercises1} />
+        <Part part={props.part2} exercises={props.exercises2} />
+        <Part part={props.part3} exercises={props.exercises3} />
       </div>
     )
   }
@@ -48,6 +42,14 @@ const Header = (props) => {
   const Total = (props) => {
     return (
         <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+        <p>
+        {props.part} {props.exercises}
+      </p>
     )
   }
 
