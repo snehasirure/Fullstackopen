a4cbcb598f08208e37d4cc238c2a4e43dcfc594a
snehasirure <siruresneha@gmail.com>
2019-10-02 00:19:11 +0200

    1.5: course information step5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -29,8 +29,7 @@ const Content = (props) => {
 const Total = (props) => {
   return(
     <div>
-        <p> Number of exercises 
-            {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+        <p> Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
         </p>
     </div>
   )
@@ -39,27 +38,29 @@ const Total = (props) => {
 
 const App = () => {
   // Constant Definitions
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header courseName={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
